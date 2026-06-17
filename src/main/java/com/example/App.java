5d46b5c2ab085296ad9edc11ee4d50434f95efe8
Vaibhavi M Kirthy <vaibsvaibhavi2@gmail.com>
2026-06-17 22:27:25 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -18,5 +18,6 @@ public class App
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+        driver.quit();
     }
 }
